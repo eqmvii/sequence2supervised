dfb56e680b183729e8adce39ccb50794e8406840
eqmvii <coelomate@gmail.com>
2018-02-13 09:11:50 -0500

    first commit of supervised sequence server

---
+++ lib/sequence2supervised.ex
@@ -0,0 +1,18 @@
+defmodule Sequence2supervised do
+  @moduledoc """
+  Documentation for Sequence2supervised.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Sequence2supervised.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/sequence2supervised_test.exs
@@ -0,0 +1,8 @@
+defmodule Sequence2supervisedTest do
+  use ExUnit.Case
+  doctest Sequence2supervised
+
+  test "greets the world" do
+    assert Sequence2supervised.hello() == :world
+  end
+end
